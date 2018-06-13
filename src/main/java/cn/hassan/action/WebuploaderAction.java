83f4a23a9fdefb74f79fa0a4abf7314e4ec5af48
hanshungithub <hshanshun@163.com>
2018-06-13 09:02:02 +0800

    调整

--- src/main/java/cn/hassan/action/WebuploaderAction.java
+++ src/main/java/cn/hassan/action/WebuploaderAction.java
@@ -1,6 +1,5 @@
 package cn.hassan.action;
 
-import cn.hassan.result.JsonResult;
 import cn.hassan.util.FileUtil;
 import cn.hassan.util.JsonUtil;
 import com.opensymphony.xwork2.ActionSupport;
@@ -50,39 +49,6 @@ public class WebuploaderAction extends ActionSupport {
 		return null;
 	}
 
-	public String ajaxAttachUpload2() {
-		String path =  "F:\\webupload\\"+multiFileFileName;
-		/**
-		 * TODO:可添加自己的业务逻辑实现
-		 * fileSize;//所有文件的总大小
-		 * multiFileName  ;// 文件名列表
-		 * fileSizeOneByOne;//每个文件大小
-		 *
-		 * 后台已经可以拿到这些属性，可以根据每个文件的大小，和总大小计算出  上传的进度百分比等。
-		 * 或者可以 判断文件上传的大小是否正确。是否丢字节。
-		 */
-		try {
-			//拿到当前正在上传的文件对象
-			File file = this.getMultiFile();
-			FileUtil.randomAccessFile(path, file);
-			if(chunk == null){
-				//不分片的情况
-				JsonUtil.write(JsonUtil.toJson("0"));
-			}else{
-				//分片的情况
-				//chunk 分片索引，下标从0开始
-				//chunks 总分片数
-				if (Integer.valueOf(chunk) == (Integer.valueOf(chunks) - 1)) {
-					JsonUtil.write(JsonUtil.toJson("0"));
-				} else {
-					JsonUtil.write(JsonUtil.toJson("2"));
-				}
-			}
-		} catch (Exception e) {
-			JsonUtil.write(JsonUtil.toJson("3"));
-		}
-		return null;
-	}
 
 
 	public File getFile() {
